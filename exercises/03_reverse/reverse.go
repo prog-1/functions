22b42f958cdb4a0aa0026b9fcf741386be513f60
Vladimir Satinov <vovaumni@gmail.com>
2021-11-05 14:12:05 +0200

    Program that reverse number

--- exercises/03_reverse/reverse.go
+++ exercises/03_reverse/reverse.go
@@ -1,4 +1,20 @@
 package main
 
-func main() {
+import "fmt"
+
+func reverse(n int) int {
+	var y int
+	for tmp := n; tmp != 0; tmp /= 10 {
+		y = y*10 + tmp%10
+	}
+	return y
 }
+
+func main() {
+	fmt.Print("Enter number: ")
+	var n int
+	fmt.Scan(&n)
+	n = reverse(n)
+	fmt.Println("Reversed number is:", n)
+} //input: 821 | 019 | 17 | -12 | 100 |
+//output : 128 | 1   | 71 | -21 | 1   |
