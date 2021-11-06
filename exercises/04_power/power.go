fc5cc608f8b5e24137e7bf1f5a63c272d0208d81
Sp0ozy <imolchanov210@gmail.com>
2021-11-06 12:44:45 +0200

    power with negative numbers

--- exercises/04_power/power.go
+++ exercises/04_power/power.go
@@ -2,22 +2,36 @@ package main
 
 import "fmt"
 
-func Pow(x, y uint) uint {
+func Pow(x, y int) int {
 	if y == 0 {
 		x = 1
 		return x
-	}
+	} else {
+		for tmp := x; y != 1; y-- {
+			x = x * tmp
 
-	for tmp := x; y != 1; y-- {
-		x = x * tmp
+		}
+
 	}
 	return x
 }
+
 func main() {
 	fmt.Println("Program determines x to the power of y")
 	fmt.Println("Enter x and y:")
-	var x, y uint
+	var x, y int
 	fmt.Scanln(&x, &y)
-	tmp := Pow(x, y)
-	fmt.Println("x ^ y =", tmp)
+	if y < 0 {
+		fmt.Println("Error: power below 0 couldn't be used")
+	} else if x == 0 {
+		fmt.Print("pow = 0")
+	} else {
+		tmp := Pow(x, y)
+		fmt.Println("x ^ y =", tmp)
+	}
+
 }
+
+//extra tests
+//input |0 9    |8 -9
+//output|pow = 0|Error: power below 0 couldn't be used
--- exercises/04_power/power_test.go
+++ exercises/04_power/power_test.go
@@ -5,14 +5,18 @@ import "testing"
 // TestPow tests a Pow(x, y) function, which returns integer x**y.
 func TestPow(t *testing.T) {
 	for _, tc := range []struct {
-		x, y uint
-		want uint
+		x, y int
+		want int
 	}{
 		{1, 1, 1},
+		{121, 0, 1},
 		{2, 4, 16},
 		{3, 5, 243},
 		{10, 2, 100},
 		{11, 2, 121},
+		{-5, 3, -125},
+		{-4, 4, 256},
+		{-8, -8, 121},
 	} {
 		if got := Pow(tc.x, tc.y); got != tc.want {
 			t.Errorf("Pow(%v, %v) = %v, want = %v", tc.x, tc.y, got, tc.want)
