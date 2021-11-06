4253960296d7e5c1d1e31243134dab7ba0c5403e
Sp0ozy <imolchanov210@gmail.com>
2021-11-06 12:00:00 +0200

    power done

---
+++ exercises/04_power/power.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func Pow(x, y uint) uint {
+	if y == 0 {
+		x = 1
+		return x
+	}
+
+	for tmp := x; y != 1; y-- {
+		x = x * tmp
+	}
+	return x
+}
+func main() {
+	fmt.Println("Program determines x to the power of y")
+	fmt.Println("Enter x and y:")
+	var x, y uint
+	fmt.Scanln(&x, &y)
+	tmp := Pow(x, y)
+	fmt.Println("x ^ y =", tmp)
+}
--- exercises/04_power/power_test.go
+++ exercises/04_power/power_test.go
@@ -8,8 +8,14 @@ func TestPow(t *testing.T) {
 		x, y uint
 		want uint
 	}{
-		// Test cases.
+		{1, 1, 1},
+		{2, 4, 16},
+		{3, 5, 243},
+		{10, 2, 100},
+		{11, 2, 121},
 	} {
-		// Test body.
+		if got := Pow(tc.x, tc.y); got != tc.want {
+			t.Errorf("Pow(%v, %v) = %v, want = %v", tc.x, tc.y, got, tc.want)
+		}
 	}
 }
