f39f9f5d43d7fc24fb87acc26a524a59f5658030
Sp0ozy <imolchanov210@gmail.com>
2021-11-06 11:59:22 +0200

    Reverse done (negative numbers included)

--- exercises/03_reverse/reverse.go
+++ exercises/03_reverse/reverse.go
@@ -1,4 +1,19 @@
 package main
 
+import "fmt"
+
+func reverse(a int) (y int) {
+	for tmp := a; tmp != 0; tmp /= 10 {
+		y = y*10 + tmp%10
+	}
+	return y
+}
 func main() {
+	fmt.Println("This program reverse a number.")
+	fmt.Print("Enter a number: ")
+	var a int
+	fmt.Scan(&a)
+	a = reverse(a)
+	fmt.Println("reverse is ", a)
+
 }
