f5204048070dccabe7fe516b82b02d8cd9885d5f
Richard Shashalevvich <richardsasalevic@gmail.com>
2021-11-07 11:54:07 +0200

    added new test scenarios

--- exercises/04_power/power.go
+++ exercises/04_power/power.go
@@ -2,7 +2,10 @@ package main
 
 import "fmt"
 
-func pow(a uint, b uint) uint {
+func pow(a int, b int) int {
+	if b == 0 {
+		return 1
+	}
 	res := a
 	for i := b; i > 1; i, res = i-1, res*a {
 	}
@@ -12,7 +15,7 @@ func pow(a uint, b uint) uint {
 func main() {
 	fmt.Println("This program returns power of two unsigned integers")
 	fmt.Print("Enter two number: ")
-	var a, b uint
+	var a, b int
 	fmt.Print("Pow of ", a, " and ", b, " equals ", pow(a, b))
 
 }
