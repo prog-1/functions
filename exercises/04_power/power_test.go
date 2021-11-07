b14a7518a0b64fdc1f7ce0c9ce34e7f39dfca507
Alina Aniscenko <alina065b@gmail.com>
2021-11-07 23:25:30 +0200

    power_test.go

--- exercises/04_power/power_test.go
+++ exercises/04_power/power_test.go
@@ -9,7 +9,21 @@ func TestPow(t *testing.T) {
 		want uint
 	}{
 		// Test cases.
+		{x: 0, y: 0, want: 1},
+		{x: 1, y: 1, want: 1},
+		{x: 0, y: 1, want: 0},
+		{x: 1, y: 0, want: 1},
+		{x: 0, y: 4, want: 0},
+		{x: 5, y: 0, want: 1},
+		{x: 2, y: 1, want: 2},
+		{x: 1, y: 4, want: 1},
+		{x: 3, y: 4, want: 81},
+		{x: 5, y: 2, want: 25},
+		{x: 12, y: 3, want: 1728},
 	} {
 		// Test body.
+		if got := Pow(tc.x, tc.y); got != tc.want {
+			t.Errorf("pow(%v, %v) = %v, want = %v", tc.x, tc.y, got, tc.want)
+		}
 	}
 }
