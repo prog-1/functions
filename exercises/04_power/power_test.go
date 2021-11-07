80fd8211fbae364d1ed41e5c4a9857cb92d94ca4
unknown <valerijamax@gmail.com>
2021-11-07 22:01:55 +0200

    adding power

--- exercises/04_power/power_test.go
+++ exercises/04_power/power_test.go
@@ -8,8 +8,20 @@ func TestPow(t *testing.T) {
 		x, y uint
 		want uint
 	}{
-		// Test cases.
+		{x: 0, y: 0, want: 1},
+		{x: 1, y: 0, want: 1},
+		{x: 7, y: 0, want: 1},
+		{x: 0, y: 7, want: 0},
+		{x: 0, y: 13467, want: 0},
+		{x: 2, y: 3, want: 8},
+		{x: 5, y: 7, want: 78125},
+		{x: 13, y: 8, want: 815730721},
+		{x: 1, y: 1, want: 1},
+		{x: 3, y: 3, want: 27},
+		{x: 7, y: 7, want: 823543},
 	} {
-		// Test body.
+		if got := pow(tc.x, tc.y); got != tc.want {
+			t.Errorf("pow(%v, %v) = (%v), want = (%v)", tc.x, tc.y, got, tc.want)
+		}
 	}
 }
