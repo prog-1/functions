87b55f9e11ce84231f432297fac3129ecc878b94
Alina Aniscenko <alina065b@gmail.com>
2021-11-07 22:38:59 +0200

    reverse.go

--- exercises/03_reverse/reverse.go
+++ exercises/03_reverse/reverse.go
@@ -1,4 +1,19 @@
 package main
 
+import "fmt"
+
+func reverse(n int) int {
+	y := 0
+	for x := n; x != 0; x /= 10 {
+		y = y*10 + x%10
+	}
+	return y
+}
+
 func main() {
+	fmt.Println("Enter the number:")
+	var n int
+	fmt.Scan(&n)
+	n = reverse(n)
+	fmt.Println("Reversed:", n)
 }
