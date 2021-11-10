024cc3431dfe47d73404702294e1be01e92ef222
Anastasija Voropajeva <a.voropajeva3@gmail.com>
2021-11-10 23:24:20 +0200

    changing power_test.go

--- exercises/04_power/power_test.go
+++ exercises/04_power/power_test.go
@@ -8,8 +8,15 @@ func TestPow(t *testing.T) {
 		x, y uint
 		want uint
 	}{
-		// Test cases.
+		{x: 0, y: 0, want: 1},
+		{x: 0, y: 1, want: 0},
+		{x: 1, y: 0, want: 1},
+		{x: 2, y: 3, want: 8},
+		{x: 2, y: 9, want: 512},
+		{x: 3, y: 3, want: 27},
 	} {
-		// Test body.
+		if gotx, goty := Pow(tc.x, tc.y); gotx != tc.want && goty >= 0 {
+			t.Errorf("Pow(%v, %v) = (%v), want = (%v)", tc.x, tc.y, gotx, tc.want)
+		}
 	}
 }
