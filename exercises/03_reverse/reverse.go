557a121a12f4f171caee14f4a32367329e49723c
Anastasija Voropajeva <a.voropajeva3@gmail.com>
2021-11-10 23:20:18 +0200

    Changing reverse.go

--- exercises/03_reverse/reverse.go
+++ exercises/03_reverse/reverse.go
@@ -1,4 +1,19 @@
 package main
 
+import "fmt"
+
+func reverse(n int) int {
+	var s = 0
+	for ; n != 0; n /= 10 {
+		s = s*10 + (n % 10)
+	}
+	return s
+}
+
 func main() {
+	fmt.Print("Enter number: ")
+	var n int
+	fmt.Scan(&n)
+	n = reverse(n)
+	fmt.Println("Reversed:", n)
 }
