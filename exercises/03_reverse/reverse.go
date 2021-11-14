201ac8628bc39a0ba6bc6b73dfdf4929cf86e016
Barash456 <denis.barannik@gmail.com>
2021-11-14 11:44:39 +0200

    functions commit

--- exercises/03_reverse/reverse.go
+++ exercises/03_reverse/reverse.go
@@ -1,4 +1,19 @@
 package main
 
+import "fmt"
+
+func reverse(number int) int {
+	y := 0
+	for x := number; x != 0; x /= 10 {
+		y = y*10 + x%10
+	}
+	return y
+}
+
 func main() {
+	fmt.Println("Enter the number:")
+	var num int
+	fmt.Scanln(&num)
+	num = reverse(num)
+	fmt.Println("reserve:", num)
 }
