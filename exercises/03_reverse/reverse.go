bc711a167ae6198caf2a6db4a89eeaf63e9a6629
Marija Z <marija.zunda@gmail.com>
2021-11-15 21:16:41 +0200

    adding reverse.go

--- exercises/03_reverse/reverse.go
+++ exercises/03_reverse/reverse.go
@@ -1,4 +1,19 @@
 package main
 
+import "fmt"
+
+func reverse(a int) int {
+	var b int
+	for c := a; c != 0; c /= 10 {
+		b = b*10 + c%10
+	}
+	return b
+}
+
 func main() {
+	fmt.Println("Enter the number:")
+	var a int
+	fmt.Scanln(&a)
+	a = reverse(a)
+	fmt.Println("The reversed number is:", a)
 }
