318fc7c6fa835dc4216272ce808e584d688bc464
Aleksej Pjavka <karpenadij@gmail.com>
2021-11-17 18:58:39 +0200

    sorting done

--- exercises/02_sort3/sort3.go
+++ exercises/02_sort3/sort3.go
@@ -2,7 +2,28 @@ package main
 
 import "fmt"
 
+func minmax(a, b int) (mn, mx int) {
+	return min(a, b), max(a, b)
+}
+
+func min(a, b int) int {
+	if b < a {
+		return b
+	}
+	return a
+}
+
+func max(a, b int) int {
+	if b > a {
+		return b
+	}
+	return a
+}
+
 func sort3(a, b, c int) (int, int, int) {
+	a, b = minmax(a, b)
+	b, c = minmax(b, c)
+	a, b = minmax(a, b)
 	return a, b, c
 }
 
--- exercises/03_reverse/reverse.go
+++ exercises/03_reverse/reverse.go
@@ -1,4 +1,13 @@
 package main
 
+func reverse(n int) int {
+
+	r := 0
+	for ; n != 0; n /= 10 {
+		r = n%10 + (r * 10)
+	}
+	return r
+}
 func main() {
+
 }
